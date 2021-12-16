c0618db4bf80f8a1f76a354f12131cdbbde2b5a2
blakdragan7 <blakdragan7@users.noreply.github.com>
2021-12-16 19:20:48 +0000

    version 5.7.1

--- Embrace.xcframework/ios-arm64_i386_x86_64-simulator/Embrace.framework/Headers/EmbraceConfig.h
+++ Embrace.xcframework/ios-arm64_i386_x86_64-simulator/Embrace.framework/Headers/EmbraceConfig.h
@@ -179,6 +179,14 @@
 @property(atomic, strong, readonly, nullable) NSArray *disabledUrlPatterns;
 
 /**
+ The header value to automatically use in place of x-emb-path
+ 
+ @note this will only work if x-emb-path is not present in the header. So x-emb-path is prioritized
+ */
+
+@property(atomic, strong, readonly, nullable) NSString* customRelativeHeaderValue;
+
+/**
  URLs that should not be captured.
  
  @note Plist detail
@@ -308,17 +316,5 @@
     - Defult: true
  */
 @property(atomic, assign, readonly) BOOL automaticViewCaptureEnabled;
-
-/**
-  Embrace can use the os_log stream to help categorize crash reports or find non-fatal exceptions such as CollectionView or Autolayout runtime issues.
-
-  By default this feature is off as the os_log stream can contain sensitive information.  Enable this feature if you comfortable with sharing your os_log data with Embrace.
- 
- @note Plist detail
-    - Key: ENABLE_OS_LOG
-    - Type: Boolean
-    - Defult: true
- */
-@property(atomic, assign, readonly) BOOL oslogTrackingEnabled;
 
 @end
